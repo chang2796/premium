6e3bc3cd759e1071c4c02bf56cd7bf790d0d6fd4
chang2796 <146045178+chang2796@users.noreply.github.com>
2024-11-22 17:42:11 +0000

    Update i.js

--- static/assets/js/i.js
+++ static/assets/js/i.js
@@ -61,3 +61,10 @@ function isUrl(val = "") {
   }
   return false;
 }
+ var password
+var pass1 = 'CARLI'
+password = prompt('Please enter your password.', ' ')
+if (password == pass1) alert('Password Correct! This site is made by a person.')
+else {
+ window.location = 'google.com'
+}
