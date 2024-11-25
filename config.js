052b37e7456456a47da5c39fbd3e928c51cb2b4f
chang2796 <146045178+chang2796@users.noreply.github.com>
2024-11-25 17:29:00 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
-    "gointerstellar.app": "gyatt",
+    "carli.up.railway": "password",
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    interstellar: "gyatt",
   },
 };
 
