05c342630518c262d16e2db1c4ed664e056a32d0
chang2796 <146045178+chang2796@users.noreply.github.com>
2024-11-25 17:31:39 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,7 @@ const config = {
     // Normal users can ignore this option. This is for the owner of the website.
     "carli.up.railway": "password",
   },
-  users: {
-    // You can add multiple users by doing username: 'password'.
-    interstellar: "gyatt",
+  users: {interstellar: "uzo89311",
   },
 };
 
